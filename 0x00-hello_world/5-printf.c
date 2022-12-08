9b0a8ea0fee1d139afb38b78e8e107961fd84cfb
Ablante Minda <ablanteminda354@gmail.com>
2022-12-08 12:12:47 -0800

    commit for printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+
 /**
- * main -entery
- * Return: always 0  
- */
+ *
+ * * main - Entry point
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
