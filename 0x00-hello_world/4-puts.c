1c7a2f47869aa6a23c7a9b126c220ba45d1c0146
Ablante Minda <ablanteminda354@gmail.com>
2022-12-08 11:31:16 -0800

    puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-
 /**
  *
- *  * main - Entry point
+ *  main - Entry point
  *
- *   *
+ * 
  *
- *    * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  *
- *     */
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
