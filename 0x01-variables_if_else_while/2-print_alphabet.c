f8802f3ab82284775b27976736883fd2e52023bc
Ablante Minda <ablanteminda354@gmail.com>
2022-12-09 13:28:17 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: alwayes 0
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while
+	      	(ch <= 'z') {
+			putchar (ch);
+			ch++;
+		}
+       putchar ('\n');
+       return (0);
+}
