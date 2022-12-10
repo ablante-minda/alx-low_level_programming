ce558f7d42b3c763140b90c11e32bce5fa43700c
Ablante Minda <ablanteminda354@gmail.com>
2022-12-10 05:49:47 -0800

    alphabet task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+
+	while 	
+
+		(x <= 'z') {
+			if ((x != 'q' || x != 'e') && x <= 'z')
+				putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
