00ee029c3d2bb69d283c9b6bdb3b9c50db225b9e
Ablante Minda <ablanteminda354@gmail.com>
2022-12-10 04:50:26 -0800

    commit for print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * return alwayes 0
+ */
+int main(void)
+{
+	char x;
+	char y;
+	x = 'a';
+	y = 'A';
+	while 
+		(x <= 'z'){
+			putchar(x);
+			x++;
+		}
+	while
+		(y <= 'Z'){
+			putchar(y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
