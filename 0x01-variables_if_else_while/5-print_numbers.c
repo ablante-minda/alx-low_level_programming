ee6617019758a625eaf65336ea771b4fa9f00997
Ablante Minda <ablanteminda354@gmail.com>
2022-12-10 06:58:38 -0800

    commit for number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	putchar ('\n');
+
+	return (0);
+}
