a98c1ee2ad997ebd1b12f4fd4a280c344f0d528c
Ablante Minda <ablanteminda354@gmail.com>
2022-12-11 09:09:59 -0800

    commit for print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	int x;
+
+	ch = 'a';
+	x = 0;
+	while
+		(x <  10) {
+			putchar(x + '0');
+			x++;
+		}
+	while
+		(ch <= 'f') {
+			putchar(ch);
+			ch++;
+		}
+	putchar('\n');
+	return (0);
+}
