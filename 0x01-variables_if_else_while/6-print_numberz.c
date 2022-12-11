83c1e342cb14e9a30ece923e99afbb100f497fef
Ablante Minda <ablanteminda354@gmail.com>
2022-12-11 08:38:44 -0800

    commit for print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while
+		(num < 10) {
+			putchar(num + '0');
+			num++;
+		}
+	putchar('\n');
+	return (0);
+}
+
