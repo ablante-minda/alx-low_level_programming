0bbcd395f42200673191f84a133a1d95a51a7441
Ablante Minda <ablanteminda354@gmail.com>
2022-12-11 08:51:43 -0800

    commit for print tebaphla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while
+		(ch >= 'a') {
+			putchar(ch);
+			ch--;
+		}
+	putchar('\n');
+	return (0);
+}
