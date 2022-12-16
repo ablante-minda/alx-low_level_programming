242fadb0b39ac817e40952c83ce59e6a62f1d8d0
Ablante Minda <ablanteminda354@gmail.com>
2022-12-16 13:25:26 -0800

    commit for debugging task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - infinite loop
+ * Returm: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop comming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o\/\n");
+
+	return (0);
+}
