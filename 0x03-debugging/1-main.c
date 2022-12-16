3f54df08e0378605abcd5bc93e13422a91dfd4fb
Ablante Minda <ablanteminda354@gmail.com>
2022-12-16 15:25:02 -0800

    commit for task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - infinite loop
- * Returm: always 0
+ * main - causes an infinite loop
+ * Return: 0
  */
 
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 	printf("Infinite loop comming :(\n");
 
 	i = 0;
-	/**
+	/*
 	 * while (i < 10)
 	 * {
 	 * putchar(i);
