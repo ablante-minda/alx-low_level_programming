f2c696f4f609229fe198c793be2e5e6dd61be251
Ablante Minda <ablanteminda354@gmail.com>
2022-12-19 13:19:12 -0800

    commit for task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copiar
+ * 2dest: this is destiny
+ * @src: copia
+ * Return: return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
