b8057e1fa556545719521bcee8911fe7833674c3
Ablante Minda <ablanteminda354@gmail.com>
2022-12-19 12:28:35 -0800

    commit for task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,11 @@ void print_rev(char *s)
 {
 	int index;
 
-	for (index = 0; s[index] != '\n'; index++)
+	for (index = 0; s[index] != '\0'; index++)
 		;
 	for (index = index - 1; s[index] != '\0'; index--)
 	{
 		_putchar(s[index]);
 	}
-	-putchar('\n');
+	_putchar('\n');
 }
