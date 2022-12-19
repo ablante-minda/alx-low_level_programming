578ac2858401488b507d6bc9de4b7f9bbfc9a905
Ablante Minda <ablanteminda354@gmail.com>
2022-12-19 09:53:38 -0800

    commit for task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,8 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter and
  * updates the value it points to to 98
- * on: This is the entry
+ * on: This is the entr
+ * 
  */
 
 void reset_to_98(int *n)
