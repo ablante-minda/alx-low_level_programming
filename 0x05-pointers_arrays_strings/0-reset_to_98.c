8bdbf7b3fd7593eaf0904e8a67fc1d6f0640be01
ablante-minda <118987951+ablante-minda@users.noreply.github.com>
2022-12-19 21:21:44 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as a parameter and
  * updates the value it points to 98 n
+ * 
  * on: This is the entry 
  */
 void reset_to_98(int *n)
