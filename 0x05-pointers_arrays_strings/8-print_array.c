c247ad0373a58775ca7a5d186b68f01cdba05e3c
Ablante Minda <ablanteminda354@gmail.com>
2022-12-19 13:10:00 -0800

    commit for task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print elements of array
+ * @a: pointer to array
+ * @n: number of array elemnts
+ *
+ * Return: void. 
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
