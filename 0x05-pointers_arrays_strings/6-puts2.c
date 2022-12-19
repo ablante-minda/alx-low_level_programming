4d31c6aaeadbfe3430bd64096213c378f7952112
Ablante Minda <ablanteminda354@gmail.com>
2022-12-19 12:46:35 -0800

    commit for task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - other char in string
+ * @str: pointer to string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
