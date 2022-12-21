6b42944ced95ce8df1f5d69308a1a38246a176db
Ablante Minda <ablanteminda354@gmail.com>
2022-12-21 05:45:22 -0800

    commit for task 01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _alphabet - print alphabets
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - for check
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
