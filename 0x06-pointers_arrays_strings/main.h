23ea037b4cde35cfcb83690e3ecd7881c9aa83ce
Ablante Minda <ablanteminda354@gmail.com>
2022-12-22 12:02:07 -0800

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,6 @@
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
-int _putchar(char c);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
