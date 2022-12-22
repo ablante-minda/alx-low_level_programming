2cdbbc2753edae7d67612eab62e77bc357b2962d
Ablante Minda <ablanteminda354@gmail.com>
2022-12-22 00:08:28 -0800

    commit for task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @t: The integer to be computed
+ * Return: the absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
