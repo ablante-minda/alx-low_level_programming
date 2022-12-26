e83449fe50746004a41562902eacb2d1ce7ff58c
Ablante Minda <ablanteminda354@gmail.com>
2022-12-26 11:52:46 -0800

    commit for task 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply 2 int
+ * @a: first intiger
+ * @b: second intiger
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
