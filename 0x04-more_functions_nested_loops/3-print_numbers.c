f5a0e57e8222cfa472bbd92d2c27e342bc063b0e
Ablante Minda <ablanteminda354@gmail.com>
2022-12-26 22:02:40 -0800

    commit for task 3 print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,18 @@
 #include "main.h"
 /**
  * print_numbers - print numbers 0 to 9
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 10; num++)
-		_putchar(num + '0');
-	_putchar('\n');
+	num = 0;
+	while (num < 10)
+	{
+		_putchar (num + '0');
+		num++;
+	}
+
+	_putchar ('\n');
 }
