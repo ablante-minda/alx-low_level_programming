7ce1579f17af1e34fb0bb1b1de92640724b15438
Ablante Minda <ablanteminda354@gmail.com>
2023-01-03 04:40:30 -0800

    commit for task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,6 @@
  * _strbrk - prints the consecutive caracters of s1 that are in s2.
  * @s: source string
  * @accept: searching string
- *
  * Return: new string.
  */
 char *_strpbrk(char *s, char *accept)
