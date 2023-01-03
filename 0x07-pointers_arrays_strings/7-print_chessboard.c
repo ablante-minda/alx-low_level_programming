bf2de135c8aca75c424c533f6c6e8816afa76fcc
Ablante Minda <ablanteminda354@gmail.com>
2023-01-03 05:09:39 -0800

    commit for chesboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - entery point
+ * @: array
+ * Return: Always 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
