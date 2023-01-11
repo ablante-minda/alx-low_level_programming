372a3ed25e9f88f9ef84e60be55d3e4c3ab080b3
Ablante Minda <ablanteminda354@gmail.com>
2023-01-11 07:18:57 -0800

    commit for task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print all the argument that a programe receives.
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * 
+ * Return: Alwayes (0) sucess.
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
