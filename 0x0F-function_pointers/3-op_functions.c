9de7e635e712dc82215b5a1fe3b366c17d7375ae
Ablante Minda <ablanteminda354@gmail.com>
2023-01-22 12:44:45 -0800

    commit for task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "3-calc.h"
+/**
+ * op_add - return the sum of 2 intiger
+ * @a: intiger 
+ * @b: intiger
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub -returns difference of 2 numbers
+ * @a: intiger 
+ * @b: intiger
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b); 
+}
+
+/**
+ * op_mul - returns product of two number
+ * @a: intiger
+ * @b: intiger
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	 return (a * b);
+}
+
+/**
+ * op_div - returns the quotient of 2 numbers
+ * @a: intiger
+ * @b: intiger
+ *
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of division
+ * @a: intiger
+ * @b: intiger
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b) 
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
