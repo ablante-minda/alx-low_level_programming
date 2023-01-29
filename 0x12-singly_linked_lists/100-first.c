59f486ad8c030b2a96134175b9039a1b070f4969
Ablante Minda <ablanteminda354@gmail.com>
2023-01-29 01:03:55 -0800

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
  */
 void __attribute__((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
