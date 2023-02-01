d150e316f99dae3f95386f284e8969b2d8c319b2
Ablante Minda <ablanteminda354@gmail.com>
2023-02-01 04:27:44 -0800

    commit for task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts anew node to a listint_t
+ * @head: a pointer to the address of the head of the listint_t list
+ * @idx: the index of the listint_t 
+ * @n: the intiger for the new node to contain
+ *
+ * Return: if the function fails - NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *copy = *head;
+	unsigned int node;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = copy;
+		*head = new;
+		return (new);
+	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
+
+		copy = copy->next;
+	}
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
+}
